a7a57e7fd8a055fd3f1ff38df907c0e9c07a33db
Irina Markov <irina@aquilatech.com>
2020-08-24 13:43:14 -0500

    initial commit

---
+++ FirstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram{
+    //Entry Point Method
+    public static void main(String[] args){ //print main
+        System.out.println("Hello World!"); //print sysout
+        byte myByte = 100; //Stores whole numbers from -128 to 128
+        short myShort = 1000; //Stores whole numbers from -32,368 to 32, 707
+        int myInt = 1000000; //Stores whole numbers from -2,147,483,648 to 2,147,....etc
+
+    }
+}
