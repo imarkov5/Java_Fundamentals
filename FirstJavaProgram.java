ecfad939977fc9e4ef0b85a72258803397efd5d3
Irina Markov <irina@aquilatech.com>
2020-08-25 22:10:33 -0500

    worked on printing in console

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Irina Markov");
+        System.out.println("I'm 45 years old");
+        System.out.println("My hometown is Bellevue, WA");
+    }
+}
