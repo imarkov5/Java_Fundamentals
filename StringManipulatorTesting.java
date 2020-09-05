759991de90e3c4d1cb8194011631acc65c394c3c
Irina Markov <irina@aquilatech.com>
2020-09-04 20:45:42 -0500

    concatinate substring, get index ot null

--- StringManipulatorTesting.java
+++ StringManipulatorTesting.java
@@ -3,5 +3,29 @@ public class StringManipulatorTesting{
         StringManipulator manipulator = new StringManipulator();
         String str = manipulator.trimAndConcat("      Hello     ","      World     ");
         System.out.println(str);
+
+        StringManipulator manipulator1 = new StringManipulator();
+        char letter = 'o';
+        Integer a = manipulator1.getIndexOrNull("Coding", letter);
+        Integer b = manipulator1.getIndexOrNull("Hello World", letter);
+        Integer c = manipulator1.getIndexOrNull("Hi", letter);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        StringManipulator manipulator2 = new StringManipulator();
+        String word = "Hello";
+        String subString = "llo";
+        String notSubString = "world";
+        Integer d = manipulator2.getIndexOrNull(word, subString);
+        Integer e = manipulator2.getIndexOrNull(word, notSubString);
+        System.out.println(d);
+        System.out.println(e);
+
+        StringManipulator manipulator3 = new StringManipulator();
+        String word1 = manipulator3.concatSubstring("Hello", 1, 2, "world");
+        System.out.println(word1);
+
+
     }
 }
