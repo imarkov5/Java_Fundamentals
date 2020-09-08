54f10269d22a4024a46a07192f87cb838e80806b
Irina Markov <irina@aquilatech.com>
2020-09-07 21:49:19 -0500

    printing alphabet, random

---
+++ Puzzling.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Puzzling {
+    public static void main(final String[] args) {
+
+        Random r = new Random();
+
+        ArrayList<Character> alphabet = new ArrayList<Character>();
+        for(char ch = 'a'; ch <= 'z'; ++ch){
+            alphabet.add(ch);
+        }
+        System.out.println(alphabet);
+        Collections.shuffle(alphabet);
+        System.out.println(alphabet);
+        System.out.println(alphabet.get(25));
+
+        int randomNumber = r.nextInt(25);
+        System.out.println(alphabet.get(randomNumber));
+    }
+}
