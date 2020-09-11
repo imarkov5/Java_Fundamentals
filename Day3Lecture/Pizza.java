29f63b4530e07157d1ecb250c260a8f5e66b102f
Irina Markov <irina@aquilatech.com>
2020-09-10 22:32:50 -0500

    constructor

--- Day3Lecture/Pizza.java
+++ Day3Lecture/Pizza.java
@@ -1,6 +1,4 @@
-package Day3Lecture;
 import java.util.Arrays;
-
 
 public class Pizza {
     public String sauce;
@@ -8,10 +6,18 @@ public class Pizza {
     public char size;
     public double price;
 
+    //Constructor
+    public Pizza(String sauce, String[] toppings, char size, double price){
+        this.sauce = sauce; //this sauce - sauce from class Pizza is going to equal to sauce that user is going to put in)
+        this.toppings = toppings;
+        this.size = size;
+        this.price = price;
+    }
+
     //Things we can do with pizza
     public void displayPizza(){
         //display pizza
-        System.out.println();
+        System.out.printf("This is a pizza with %s sauce, and with %s toppings, that costs $%.2f dollars", this.sauce, Arrays.toString(this.toppings), this.price);
     }
 
     //eat a slice
--- Day3Lecture/PizzaController.java
+++ Day3Lecture/PizzaController.java
@@ -1,13 +1,14 @@
-package Day3Lecture;
-
 public class PizzaController {
     //Entry Point Method
     public static void main(String[] args) {
-        Pizza cheesePizza = new Pizza();
-        cheesePizza.toppings = new String[]{"cheese"};
-        cheesePizza.sauce = "marinara";
-        cheesePizza.size = 'L';
-        cheesePizza.price = 14.99;
+        // Pizza cheesePizza = new Pizza();
+        // cheesePizza.toppings = new String[]{"cheese"};
+        // cheesePizza.sauce = "marinara";
+        // cheesePizza.size = 'L';
+        // cheesePizza.price = 14.99;
+        // cheesePizza.displayPizza();
 
+        Pizza pepperoniPizza = new Pizza("white sauce", new String[]{"pepperoni", "cheese"}, 'L', 14.99);
+        pepperoniPizza.displayPizza();
     }
 }
