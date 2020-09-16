17a3f4eafa86db9b48a62d546acc366e892e5b00
Irina Markov <irina@aquilatech.com>
2020-09-16 13:47:31 -0500

    objects interact with each other

--- Day4Lecture/Animal.java
+++ Day4Lecture/Animal.java
@@ -2,14 +2,17 @@ public class Animal {
     private String species;
     private char gender;
     private int health;
+    private int strength;
 
     //Method Overloading
     public Animal(){ //this constructor called POJO: Plain Old Java Object - empty constructor
 
     }
-    public Animal(String species, char gender){
+    public Animal(String species, char gender, int strength){
         this.species = species;
         this.gender = gender;
+        this.health = 100;
+        this.strength = strength;
     }
 
     //Getters and Setters
@@ -25,6 +28,39 @@ public class Animal {
     public void setGender(){
         this.gender = gender;
     }
+    public int getHealth(){
+        return this.health;
+    }
+    public void setHealth(int health){
+        this.health = health;
+    }
+
+    public void battle(String powerMove, Animal target){
+        //Define the logic how the animal attacks
+        int damage;
+        if(powerMove.equals("roar")){
+            damage = 3;
+        } else if(powerMove.equals("bear hug")){
+            damage = 10;
+        } else if (powerMove.equals("defaultMove")){
+            damage = 2; 
+        }else{
+            System.out.println("Move not recognized");
+            return;
+        }
+        //multiply the strength and the damage
+        int effectiveDamage = this.strength * damage;
+
+        //reduce the target health by effectiveDamage
+        target.health -= effectiveDamage;
+
+        //print to the console what just happened
+        System.out.printf("%s attacks %s for %d health points. ", this.species, target.species, effectiveDamage);
+        System.out.println("Target Health: " + target.health);
+    }
     
+    public void battle(Animal target){
+        this.battle("defaultMove", target);
+    }
 
 }
--- Day4Lecture/AnimalController.java
+++ Day4Lecture/AnimalController.java
@@ -1,7 +1,10 @@
 public class AnimalController {
     public static void main(String[] args) {
-        Animal tiger = new Animal("Tiger", 'f');
-        Animal lion = new Animal("Lion", 'm');
-        Animal bear = new Animal("Bear", 'f');
+        Animal tigress = new Animal("Tigress", 'f', 5);
+        Animal lion = new Animal("Lion", 'm', 7);
+        Animal bear = new Animal("Bear", 'f', 10);
+
+        tigress.battle("roar", lion);
+        bear.battle(tigress);
     }
 }
